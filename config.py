5a39200144efea33a42f27d933e3ad3363de4603
Folco <folco.carril@gmail.com>
2025-06-03 21:33:51 -0300

    feat: se agrego las configuraciones del proyecto

---
+++ config.py
@@ -0,0 +1,5 @@
+HOST = '127.0.0.1'
+PORT = 12345
+
+# Menu del server
+MENU = "1 - Login\n2 - Enviar mensaje a otro usuario\n3 - Enviar mensaje a todos los usuarios conectados\n4 - Mostrar usuarios conectados\n5 - Salir"
