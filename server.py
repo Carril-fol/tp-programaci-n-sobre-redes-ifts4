b4ec6847b660a4d8e4bebfa5483a6b3440551eaf
Folco <folco.carril@gmail.com>
2025-06-05 23:35:19 -0300

    feat: se agregro threading (server)

--- server.py
+++ server.py
@@ -1,27 +1,42 @@
 import socket
+import uuid
 import threading
 from config import MENU
 
 HOST = '127.0.0.1'
 PORT = 12345
 
+clients = []
+
 class Server:
 
     def __init__(self):
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.clients = []
 
-    def _user_already_exists(self, address):
-        return address in self.clients
+    def users_online(self):
+        if len(clients) == 0:
+            return "No hay nadie conectado."
+        return "\n".join(f"\n🧑‍💻 {user["username"]}\n" for user in clients)
 
-    def _save_user_data(self, address):
-        try:
-            if self._user_already_exists(address):
-                return False
-            self.clients.append(address)
-            return True
-        except Exception as error:
-            print(f"Ocurrio un error al momento de guardar los datos: {error}")
+    def register(self, client, username: str, password: str):
+        if not username or not password:
+            raise Exception("Ingrese los datos correspondientes")
+        
+        user_id = str(uuid.uuid4())
+        data_user = {
+            "id": user_id, 
+            "username": username.lower(),
+            "password": password,
+            "client": client
+        }
+        clients.append(data_user)
+        return True
+
+    def authenticate(self, username: str, password: str):
+        for user in clients:
+            if user["username"] == username and user["password"] == password:
+                return user
+        return None
 
     def shutdown_socket(self):
         try:
@@ -30,32 +45,95 @@ class Server:
         except Exception as error:
             print(f"Ocurrio un error al apagar el server: {error}")
 
-    def handle_client(self):
+    def conversation_with_client(self, client, address):
+        # Variable de control de usuario
+        user = None
         try:
-            client_socket, address = self.socket.accept()
+            # Mientras sea verdadero...
             while True:
-                # Envio de menu al usuario
-                client_socket.send(MENU.encode("utf-8"))
+                # Envio del menu al cliente
+                client.send(MENU.encode("utf-8"))
+
+                # Respuesta del cliente recibida por el servidor
+                response = client.recv(1024).decode("utf-8").strip()
+                if not response:
+                    break
                 
-                # Mensaje enviada por el cliente
-                data = client_socket.recv(1024)
-                # Hilo del cliente
-                threading.Thread(target=client_socket)
-                print(f"Respuesta del cliente: {data.decode("utf-8")}")
-        except KeyboardInterrupt:
-            print("\nServidor detenido manualmente.")
+                if response == "1":
+                    client.send("\nIngrese su nombre de usuario".encode("utf-8"))
+                    username = client.recv(1024).decode("utf-8")
+
+                    client.send("\nIngrese cual va a ser su contraseña".encode("utf-8"))
+                    password = client.recv(1024).decode("utf-8")
+
+                    if self.register(client, username, password):
+                        client.send(f"\n¡Usuario registrado con exito!\n".encode("utf-8"))
+                    else:
+                        client.send(f"\nError al crear el usuario, pruebe otra vez...\n".encode("utf-8"))
+
+                elif response == "2":
+                    client.send("\nIngrese su nombre de usuario: ".encode("utf-8"))
+                    username = client.recv(1024).decode("utf-8")
+
+                    client.send("\nIngrese su contraseña: ".encode("utf-8"))
+                    password = client.recv(1024).decode("utf-8")
+                    
+                    # Si esta autenticado, entonces...
+                    if self.authenticate(username, password):
+                        user = username
+                        client.send(f"\nBienvenido {user}.\n".encode("utf-8"))
+                    # Si no..
+                    else:
+                        client.send("\nUsuario o contraseña incorrectos.\n".encode("utf-8"))
+
+                elif response == "3":
+                    client.send("\n¿A quien desea enviarle un mensaje?".encode("utf-8"))
+                    username_from_user_to_send = client.recv(1024).decode("utf-8")
+
+                elif response == "4":
+                    client.send("\n🟢 - Usuarios en linea:".encode("utf-8"))
+                    # Se llama a la función .users_online()
+                    online_users = self.users_online()
+                    # Se envia la información de los usuarios conectados.
+                    client.send(online_users.encode())
+
+                elif response == "5":
+                    pass
+
+                elif response == "7":
+                    client.close()
+
+            client.close()
+            print("Conexion cerrada")
         except Exception as error:
-            print(f"Ocurrió un error durante la ejecución: {error}")
+            print(f"Ocurrio un error a: {error}")
+        except KeyboardInterrupt as error:
+            print(f"Desconexion por parte del cliente")
+            self.shutdown_socket()
 
     def start(self):
         try:
+            # Setteo del socket a un HOST y un PORT
             self.socket.bind((HOST, PORT))
+            # Configuracion de escuchar del servidor
             self.socket.listen(5)
-            print("Server encendido")
+            print("🟢 - ¡Server encendido!")
+
+            while True:
+                # Manejo del cliente y su direccion IP
+                client, address = self.socket.accept()
+                print(f"- Conexión desde {address}")
+
+                # Hilo para manejar las respuestas de los clientes que se conecten al servidor
+                threading_clients = threading.Thread(target=self.conversation_with_client, args=(client, address, ))
+                threading_clients.start()
+
         except Exception as error:
             print(f"Ocurrio un error al configurar el socket: {error}")
-            self.socket.close()
-        self.handle_client()
+            self.shutdown_socket()
+        except KeyboardInterrupt as error:
+            print(f"Servidor apagado")
+            self.shutdown_socket()
 
-server = Server()
-server.start()
+a = Server()
+a.start()
