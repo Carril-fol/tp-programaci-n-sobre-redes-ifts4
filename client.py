42f14047b6c62aed166de34791d6967578819e6f
Folco <folco.carril@gmail.com>
2025-06-05 23:35:05 -0300

    feat: se agrego threading (cliente)

--- client.py
+++ client.py
@@ -1,4 +1,5 @@
 import socket
+import threading
 from config import HOST, PORT
 
 class Client:
@@ -6,37 +7,40 @@ class Client:
     def __init__(self):
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-    def connect_socket(self):
+    def send_msg(self):
         try:
-            self.socket.connect((HOST, PORT))
+            while True:
+                msg = input("# ").lower()
+                self.socket.send(msg.encode('utf-8'))
         except Exception as error:
-            print(f"Ocurrio un error al conectarse al servidor: {error}") 
-
-    def send_msg(self, msg):
+            print(f"Ocurrio un error al mandar un mensaje: {error}")
+            
+    def recieve_msg(self):
         try:
-            self.socket.send(msg.encode("utf-8"))
+            while True:
+                response = self.socket.recv(1024).decode('utf-8')
+                print(response)
         except Exception as error:
-            print(f"Ocurrio un error al enviar el mensaje al servidor: {error}") 
-
-    def received_msg_from_server(self):
-        try: 
-            data = self.socket.recv(1024)
-            print(f"Respuesta del servidor: {data.decode("utf-8")}")
-        except Exception as error:
-            print(f"Ocurrio un error al recibir el mensaje del servidor: {error}") 
+            print(f"Ocurrio un error al recibir un mensaje: {error}")
 
     def start(self):
         try:
-            self.connect_socket()
-            while True:
-                self.received_msg_from_server()
-                msg_to_send = input("# ")
-                self.send_msg(msg_to_send)
-        except Exception as error:
-            print(f"Ocurrió un error durante la ejecución: {error}")
+            # Conexión al servidor
+            self.socket.connect((HOST, PORT))
+            print("Conectado al servidor")
+
+            # Hilo de recibir informacion del servidor
+            threading.Thread(target=self.recieve_msg).start()
+
+            # Funcion que permite enviar datos al servidor
+            self.send_msg()
+        except KeyboardInterrupt:
+            print("\n[Desconectado por usuario]")
+        except Exception as e:
+            print(f"[Error] Conexión fallida: {e}")
         finally:
             self.socket.close()
+            print("[Conexión cerrada]")
 
-
-client = Client()
-client.start()
+cliente = Client()
+cliente.start()
