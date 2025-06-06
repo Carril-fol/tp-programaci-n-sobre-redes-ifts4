5c455b2889c19ce4bf25c8e0bef049bbe7bac68f
Folco <folco.carril@gmail.com>
2025-06-05 23:35:47 -0300

    style: se agregaron opciones dentro del menu del codigo

--- config.py
+++ config.py
@@ -2,4 +2,12 @@ HOST = '127.0.0.1'
 PORT = 12345
 
 # Menu del server
-MENU = "1 - Login\n2 - Enviar mensaje a otro usuario\n3 - Enviar mensaje a todos los usuarios conectados\n4 - Mostrar usuarios conectados\n5 - Salir"
+MENU = """
+1 - Registrarse\n
+2 - Login\n
+3 - Enviar mensaje a otro usuario\n
+4 - Ver los usuarios conectados\n
+5 - Enviar mensaje a todos los usuarios conectados\n
+6 - Mostrar usuarios conectados\n
+7 - Salir\n
+"""
