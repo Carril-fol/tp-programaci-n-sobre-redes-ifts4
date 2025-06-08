c096becb9661b9e6f3f22af0f4efe2757700f715
Folco <folco.carril@gmail.com>
2025-06-08 03:44:25 -0300

    feat: cambio en el menu

--- config.py
+++ config.py
@@ -5,9 +5,8 @@ PORT = 12345
 MENU = """
 1 - Registrarse\n
 2 - Login\n
-3 - Enviar mensaje a otro usuario\n
+3 - Enviar mensaje a otro usuario: [nombre] + [mensaje]\n
 4 - Ver los usuarios conectados\n
 5 - Enviar mensaje a todos los usuarios conectados\n
-6 - Mostrar usuarios conectados\n
-7 - Salir\n
+6 - Salir\n
 """
